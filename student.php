1d3c4c2184828cf12bb4d7bbd672095975161930
Donald Weidner <don@bitscopic.com>
2015-07-12 15:22:08 -0400

    mobile view and reorganizing

--- student.php
+++ student.php
@@ -1,33 +1,19 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>App Scouts</title>       
-        <link rel="stylesheet" href="css/bootstrap-3.3.5/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/custom.css">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    </head>
+    <?php include 'header.php';?>
     <body>
  
 
- 		<?php include 'header.php';?>
+ 		<?php include 'navigation.php';?>
 
 		<div class="container-fluid">
 
 			<div class="pictureHolder">
 				<div class="contentHolder">
-					<div class="row">
-						<div style="text-align:center;">
-							<a href="student.php" style="border-bottom:2px solid white;" class="topLink">Students</a>
-							<a href="school.php" class="topLink">Schools</a>
-						</div>
-					</div>
-					<div class="row" style="margin-top:30px;">
-						<div class="col-xs-4">
+					<div class="row" style="margin-top:100px;">
+						<div class="col-md-4">
 							<div class="student"></div>
 						</div>
-						<div class="col-xs-8">
+						<div class="col-md-8">
 							<div class="textHolder">
 								<h3>STUDENTS</h3>
 								<p>With App Scouts, you will have the opportunity to apply what you’ve learned in the classroom to create your own games and apps.</p>
